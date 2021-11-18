1b5424b921412c7036e01b9adfbe5e9661cd3f1d
54254 <54254@etu.he2b.be>
2021-11-18 21:38:09 +0100

    Adds a list of heroes and display it, also adds click event on a hero by displaing the hero's details

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,14 +9,17 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero : Hero ={
-    id:1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSelect(hero: Hero):void {
+    this.selectedHero = hero;
+  }
+
 
 }
