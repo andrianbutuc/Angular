9235ae66232af8bdca27c32bee739e739edce853
54254 <54254@etu.he2b.be>
2021-11-21 17:46:13 +0100

    Adds delete hero option

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -58,4 +58,18 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl,hero,this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  deleteHero(id: number):Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url,this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -20,8 +20,18 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(
-        heroes => this.heroes = heroes
-      );
+      heroes => this.heroes = heroes
+    );
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    // the subscribe() callback receives the new hero and pushes it into to the heroes list for display.
+    this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero); });
   }
 
+  delete(hero: Hero):void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
